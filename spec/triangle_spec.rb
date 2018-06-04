2fb96a48eedaa964f4b6ec04fe756cfdea8c3206
hokupod <fireface800rme@gmail.com>
2018-06-04 23:40:06 +0900

    Add all testcase

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,8 +3,23 @@ require './triangle'
 
 describe Triangle do
   describe '#shape' do
-    let(:edges) { [1, 1, 1] }
-    subject { Triangle.shape edges }
-    it { is_expected.to eq '不等辺三角形ですね！' }
+    subject { triangle.shape }
+    let(:triangle) { Triangle.new *sides }
+    context 'Unequal triangles' do
+      let(:sides) { [2, 3, 4] }
+      it { is_expected.to eq '不等辺三角形ですね！' }
+    end
+    context 'Isosceles triangles' do
+      let(:sides) { [2, 2, 1] }
+      it { is_expected.to eq '二等辺三角形ですね！' }
+    end
+    context 'Regular triangle' do
+      let(:sides) { [1, 1, 1] }
+      it { is_expected.to eq '正三角形ですね！' }
+    end
+    context 'Not a triangle' do
+      let(:sides) { [1, 2, 3] }
+      it { is_expected.to eq '三角形じゃないです＞＜' }
+    end
   end
 end

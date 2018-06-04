44b4ecd881a4a90e04263eef902d189d63ac610a
hokupod <fireface800rme@gmail.com>
2018-06-04 23:42:27 +0900

    Implement triangle judgment.

--- triangle.rb
+++ triangle.rb
@@ -1,7 +1,34 @@
 class Triangle
-  class << self
-    def shape(*edges)
-      '不等辺三角形ですね！'
+  attr_reader :side_a, :side_b, :side_c, :sides
+
+  def initialize(*args)
+    @side_a, @side_b, @side_c = @sides = args.map(&:to_i)
+    raise ArgumentError '三角形じゃないです＞＜' if sides.include?(0)
+  end
+
+  def shape
+    return '正三角形ですね！' if equilateral?
+    return '二等辺三角形ですね！' if isosceles?
+    return '不等辺三角形ですね！' if unequal?
+    '三角形じゃないです＞＜'
+  end
+
+  def equilateral?
+    [side_a, side_b, side_c].uniq.one?
+  end
+
+  def isosceles?
+    return false unless [side_a, side_b, side_c].uniq.count == 2
+    return false if sides.min * 2 != sides.max
+    true
+  end
+
+  def unequal?
+    return false unless [side_a, side_b, side_c].uniq.count == 3
+    sum_combinations = sides.combination(2).to_a.map { |combination| combination.inject(0, &:+) }
+    sum_combinations.each do |sum_combination|
+      return false if sides.include?(sum_combination)
     end
+    true
   end
 end
